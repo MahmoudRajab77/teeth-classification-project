3f48e342adf680f632e2e6c0a544d7136ae69490
Mahmoud Amin <45555722+MahmoudRajab77@users.noreply.github.com>
2026-02-14 10:20:33 +0200

    Create config.py

---
+++ Pre-Trained_Model/src/config.py
@@ -0,0 +1,19 @@
+import os
+
+
+class Config:
+    
+    #data paths
+    DATA_DIR  = '/content/drive/MyDrive/Teeth_Dataset'
+    TRAIN_DIR = os.path.join(DATA_DIR, 'Training')
+    TEST_DIR  = os.path.join(DATA_DIR, 'Testing')
+    VAL_DIR   = os.path.join(DATA_DIR, 'Validation')
+    
+    
+    # Image Processing
+    IMG_HEIGHT = 224
+    IMG_WIDTH  = 224
+    BATCH_SIZE = 32
+    
+    # Model
+    NUM_CLASSES = 7
