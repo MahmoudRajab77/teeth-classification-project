fbe3a7b3050ef813144c4a8e833c81226b3b762f
Mahmoud Amin <45555722+MahmoudRajab77@users.noreply.github.com>
2026-02-14 11:34:08 +0200

    Update pretrained_model.py

--- Pre-Trained_Model/src/pretrained_model.py
+++ Pre-Trained_Model/src/pretrained_model.py
@@ -30,7 +30,7 @@ def create_pretrained_model(input_shape=(244,244,3), num_classes=7):
     layers.Input(shape = input_shape),              # first layer (input layer)
     layers.Lambda(lambda x: (x - 0.5) * 2),         # A layer Convert our image values from [0, 1] to [-1, 1] using lambda to fit the MobileNetV2 input
     base_model,                                      # the third layer 
-    layers.BatchNormilzation(),                     # Fourth layer
+    layers.BatchNormalization(),                     # Fourth layer
     layers.Dropout(0.3),                            # Drop 30% of cells randomly during the training to prevent overfitting 
     layers.Dense(256, activation = 'relu'),         # a layer of 256 neurons 
     layers.Dropout(0.3),                            # Dropout layer again 
